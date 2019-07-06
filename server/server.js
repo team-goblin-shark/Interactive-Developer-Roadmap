fa19709692182da08805db3c38c852cc0c61ff12
EP <ep@github.com>
2019-07-06 12:39:51 -0400

    dummy data and middleware setup
    Co-authored-by: Rocky <rliao3613@gmail.com>

--- server/server.js
+++ server/server.js
@@ -8,17 +8,17 @@ const port = 3000;
 
 const conString = 'postgres://kupqjigy:vzZUKkBtNXGMFp-r8jpmUdRp7L1_JJtY@raja.db.elephantsql.com:5432/kupqjigy';
 const client = new pg.Client(conString);
-client.connect((err) => {
-  if (err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', (err, result) => {
-    if (err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-  });
-});
+// client.connect((err) => {
+//   if (err) {
+//     return console.error('could not connect to postgres', err);
+//   }
+//   client.query('SELECT NOW() AS "theTime"', (err, result) => {
+//     if (err) {
+//       return console.error('error running query', err);
+//     }
+//     console.log(result.rows[0].theTime);
+//   });
+// });
 
 
 app.get('/', (req, res) => res.send('Hello World!'));
