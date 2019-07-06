b22691bc353e5a7eeff1917fdde5a73ebaa8140a
juanhart1 <juanhart1@gmail.com>
2019-07-06 16:43:16 -0400

    Committed before pushing up MVP

--- server/server.js
+++ server/server.js
@@ -9,8 +9,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+<<<<<<< HEAD
 app.get('/api/resources/:id', getData);
 
 app.get('/api/category', getCategory);
 
+=======
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.get('/fakeData', fakeData);
+// console.log(process.env);
+app.get('/api/resources/:id', getData);
+
+app.get('/api/category', getCategory);
+
+>>>>>>> 923dc58c2e56540c92353e699f3809fffaeb3d9d
 app.listen(port, () => console.log(`listening on port ${port}!`));
