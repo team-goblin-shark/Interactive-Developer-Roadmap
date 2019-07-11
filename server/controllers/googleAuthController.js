0db83ef20eb0ceb7c9264c630a0f6b9b76becc98
Josh Kim <joshua940308@gmail.com>
2019-07-11 10:58:39 -0400

    fixed router error

--- server/controllers/googleAuthController.js
+++ server/controllers/googleAuthController.js
@@ -11,6 +11,11 @@ const {
 const { cookieSecret } = require('../server_settings/oAuthSettings.js');
 
 const oauth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_url);
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'online',
+  scope: scopes,
+});
+console.log('redirecting to', url);
 
 const googleAuthController = {
   getGooglePicture: (req, res) => {
@@ -23,36 +28,19 @@ const googleAuthController = {
       res.status(200).send({ picture: result.picture });
     });
   },
-  redirectToGoogleSignOn: (req, res, next) => {
-    // generating oauth url that directs client to login through google
-    const url = oauth2Client.generateAuthUrl({
-      access_type: 'online',
-      scope: scopes,
-    });
-    console.log('redirecting to', url);
-    res.redirect(url);
-    // request.post(url, (error, response, body) => {
-    //   if (error) {
-    //     console.error(error);
-    //     return res.status(400).send({ message: 'error in post request' });
-    //   }
-    //   console.log(response);
-    //   return next();
-    // });
-  },
   getGoogleAuthCode: (req, res, next) => {
     const { code } = req.query;
     res.locals.code = code;
-    // console.log('googleAuthController => getAuthCode', code);
-    next();
+    console.log('googleAuthController => getAuthCode', code);
+    return next();
   },
   getGoogleToken: async (req, res, next) => {
     const { code } = res.locals;
     const { tokens } = await oauth2Client.getToken(code);
-    // console.log('googleAuthController => getGoogleToken => tokens', tokens);
+    console.log('googleAuthController => getGoogleToken => tokens', tokens);
     oauth2Client.setCredentials(tokens);
     res.locals.tokens = tokens;
-    next();
+    return next();
   },
   getGoogleEmail: async (req, res, next) => {
     const oauth2 = google.oauth2({
@@ -70,14 +58,6 @@ const googleAuthController = {
       console.log(res.locals.picture);
       return next();
     });
-    // const plus = google.plus({ version: 'v1', oauth2Client });
-    // let me;
-    // try {
-    //   me = await plus.people.get({ userId: 'me' });
-    // } catch (err) {
-    //   console.error(err);
-    // }
-    // console.log('googleAuthController => getGoogleEmail', me);
   },
 };
 
